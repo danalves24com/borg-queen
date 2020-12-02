856c42ac54167a82cac79b2feb7215016a393218
Daniel Rosel <danalves24@outlook.com>
2020-12-02 12:18:26 +0100

    8539

--- src/server.js
+++ src/server.js
@@ -15,11 +15,7 @@ app.get("/stat", (req, res) => {
 })
 
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
 const server = new WebSocket.Server({
   port: 8080
 });
